eb71157cd8b1affdd06e141ffcd47a0b4ca2a927
Leslie <wilson.leslie2021@gmail.com>
2025-10-07 23:51:56 -0400

    Adding data for API

---
+++ data/stations.js
@@ -0,0 +1,160 @@
+const stations = [
+    {
+        stateId: 1, 
+        stateName: Alabama,
+        radioStation: "https://radiostationusa.fm/online/951-the-fox"
+    },
+    {
+        stateId: 2, 
+        stateName: Alaska,
+        radioStation: "https://radiostationusa.fm/online/1039-ted-fm"
+    },
+    {
+        stateId: 3, 
+        stateName: Arizona,
+        radioStation: "https://radiostationusa.fm/online/kool"
+    },
+    {
+        stateId: 4, 
+        stateName: Arkansas,
+        radioStation: "https://radiostationusa.fm/online/cool-1047"
+    },
+    {
+        stateId: 5, 
+        stateName: California,
+        radioStation: "https://radiostationusa.fm/online/kool-959"
+    },
+    {
+        stateId: 6, 
+        stateName: Colorado,
+        radioStation: "https://radiostationusa.fm/online/the-fox"
+    },
+    {
+        stateId: 7, 
+        stateName: Connecticut,
+        radioStation: "https://radiostationusa.fm/online/991-plr"
+    },
+        
+    {
+        stateId: 8, 
+        stateName: Delaware,
+        radioStation: "https://radiostationusa.fm/online/the-vault-rock"
+    },
+        
+    {
+        stateId: 9, 
+        stateName: Florida,
+        radioStation: "https://radiostationusa.fm/online/1027-the-beach"
+    },
+
+    {
+        stateId: 10, 
+        stateName: Georgia,
+        radioStation: "https://radiostationusa.fm/online/cruisin-94"
+    },    
+    {
+        stateId: 11, 
+        stateName: Hawaii,
+        radioStation: "https://radiostationusa.fm/online/1015-k-rock-hawaii"
+    },    
+    {
+        stateId: 12, 
+        stateName: Idaho,
+        radioStation: "https://radiostationusa.fm/online/959-klzx"
+    },    
+    {
+        stateId: 13, 
+        stateName: Illinois,
+        radioStation: "https://radiostationusa.fm/online/957-the-rock"
+    },
+        
+    {
+        stateId: 14, 
+        stateName: Indiana,
+        radioStation: "https://radiostationusa.fm/online/wxke"
+    },    
+    {
+        stateId: 15, 
+        stateName: Iowa,
+        radioStation: "https://radiostationusa.fm/online/1057-kokz"
+    },    
+    {
+        stateId: 16, 
+        stateName: Kansas,
+        radioStation: "https://radiostationusa.fm/online/kkci-102-5"
+    },    
+    {
+        stateId: 17, 
+        stateName: Kentucky,
+        radioStation: "https://radiostationusa.fm/online/weky"
+    },    
+    {
+        stateId: 18, 
+        stateName: Louisiana,
+        radioStation: "https://radiostationusa.fm/online/rock-977"
+    },    
+    {
+        stateId: 19, 
+        stateName: Maine,
+        radioStation: "https://radiostationusa.fm/online/1029-wblm"
+    },    
+    {
+        stateId: 20, 
+        stateName: Maryland,
+        radioStation: "https://radiostationusa.fm/online/1007-the-bay"
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },    
+    {
+        stateId: 7, 
+        stateName: Alaska,
+        radioStation: ""
+    },
+
+
+
+
+
+
+
+
+
+]
