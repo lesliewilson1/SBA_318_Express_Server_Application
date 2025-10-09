da904c2e612589f244496d384a70c6ce02e9e0d1
Leslie <wilson.leslie2021@gmail.com>
2025-10-09 10:29:30 -0400

    Added Type:Module

--- data/stations.js
+++ data/stations.js
@@ -1,146 +1,146 @@
 const stations = [
     {
         stateId: 1, 
-        stateName: Alabama,
+        stateName: "Alabama",
         radioStation: "https://radiostationusa.fm/online/951-the-fox"
     },
     {
         stateId: 2, 
-        stateName: Alaska,
+        stateName: "Alaska",
         radioStation: "https://radiostationusa.fm/online/1039-ted-fm"
     },
     {
         stateId: 3, 
-        stateName: Arizona,
+        stateName: "Arizona",
         radioStation: "https://radiostationusa.fm/online/kool"
     },
     {
         stateId: 4, 
-        stateName: Arkansas,
+        stateName: "Arkansas",
         radioStation: "https://radiostationusa.fm/online/cool-1047"
     },
     {
         stateId: 5, 
-        stateName: California,
+        stateName: "California",
         radioStation: "https://radiostationusa.fm/online/kool-959"
     },
     {
         stateId: 6, 
-        stateName: Colorado,
+        stateName: "Colorado",
         radioStation: "https://radiostationusa.fm/online/the-fox"
     },
     {
         stateId: 7, 
-        stateName: Connecticut,
+        stateName: "Connecticut",
         radioStation: "https://radiostationusa.fm/online/991-plr"
     },
         
     {
         stateId: 8, 
-        stateName: Delaware,
+        stateName: "Delaware",
         radioStation: "https://radiostationusa.fm/online/the-vault-rock"
     },
         
     {
         stateId: 9, 
-        stateName: Florida,
+        stateName: "Florida",
         radioStation: "https://radiostationusa.fm/online/1027-the-beach"
     },
 
     {
         stateId: 10, 
-        stateName: Georgia,
+        stateName: "Georgia",
         radioStation: "https://radiostationusa.fm/online/cruisin-94"
     },    
     {
         stateId: 11, 
-        stateName: Hawaii,
+        stateName: "Hawaii",
         radioStation: "https://radiostationusa.fm/online/1015-k-rock-hawaii"
     },    
     {
         stateId: 12, 
-        stateName: Idaho,
+        stateName: "Idaho",
         radioStation: "https://radiostationusa.fm/online/959-klzx"
     },    
     {
         stateId: 13, 
-        stateName: Illinois,
+        stateName: "Illinois",
         radioStation: "https://radiostationusa.fm/online/957-the-rock"
     },
         
     {
         stateId: 14, 
-        stateName: Indiana,
+        stateName: "Indiana",
         radioStation: "https://radiostationusa.fm/online/wxke"
     },    
     {
         stateId: 15, 
-        stateName: Iowa,
+        stateName: "Iowa",
         radioStation: "https://radiostationusa.fm/online/1057-kokz"
     },    
     {
         stateId: 16, 
-        stateName: Kansas,
+        stateName: "Kansas",
         radioStation: "https://radiostationusa.fm/online/kkci-102-5"
     },    
     {
         stateId: 17, 
-        stateName: Kentucky,
+        stateName: "Kentucky",
         radioStation: "https://radiostationusa.fm/online/weky"
     },    
     {
         stateId: 18, 
-        stateName: Louisiana,
+        stateName: "Louisiana",
         radioStation: "https://radiostationusa.fm/online/rock-977"
     },    
     {
         stateId: 19, 
-        stateName: Maine,
+        stateName: "Maine",
         radioStation: "https://radiostationusa.fm/online/1029-wblm"
     },    
     {
         stateId: 20, 
-        stateName: Maryland,
+        stateName: "Maryland",
         radioStation: "https://radiostationusa.fm/online/1007-the-bay"
     },    
     {
         stateId: 21, 
-        stateName: Massachusetts,
+        stateName: "Massachusetts",
         radioStation: "https://radiostationusa.fm/online/wzlx"
     },    
     {
         stateId: 22, 
-        stateName: Michigan,
+        stateName: "Michigan",
         radioStation: "https://radiostationusa.fm/online/wcsx"
     },    
     {
         stateId: 23, 
-        stateName: Minnesota,
+        stateName: "Minnesota",
         radioStation: "https://radiostationusa.fm/online/93x"
     },    
     {
         stateId: 24, 
-        stateName: Mississippi,
+        stateName: "Mississippi",
         radioStation: "https://radiostationusa.fm/online/z1067"
     },    
     {
         stateId: 25, 
-        stateName: Missouri,
+        stateName: "Missouri",
         radioStation: "https://radiostationusa.fm/online/classic-rock-931"
     },    
     {
         stateId: 26, 
-        stateName: Montana,
+        stateName: "Montana",
         radioStation: "https://radiostationusa.fm/online/1037-the-hawk"
     },    
     {
         stateId: 27, 
-        stateName: Nebraska,
+        stateName: "Nebraska",
         radioStation: "https://radiostationusa.fm/online/z92"
     },    
     {
         stateId: 28, 
-        stateName: Nevada,
+        stateName: "Nevada",
         radioStation: "https://radiostationusa.fm/online/97-1-the-point"
     },    
     {
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express'
+
 const app = express();
-const port = 3000;
+
+const port = 1025;
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}.`);
