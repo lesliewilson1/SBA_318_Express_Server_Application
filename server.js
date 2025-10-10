7fc4408971fed9d68c28b09ce3eac30fd7054999
Leslie <wilson.leslie2021@gmail.com>
2025-10-10 01:14:32 -0400

    Update 1.0

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Importing 
 import express from 'express'
 import stations from './data/stations.js';
 import bodyParser from 'body-parser';
@@ -7,9 +8,29 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+//GET example
+app.get('/stations', (req, res) => {
     res.json(stations);
 })
+
+//POST example
+app.post('/stations', (req, res) => {
+    stations.push(req.body);
+    res.json(stations);
+})
+
+//PATCH example
+app.patch('/stations/:updates', (req, res) => {
+    stations[req.params.updates] = req.body;
+    res.json(stations);
+})
+
+//DELETE example
+app.delete('/stations/:updates', (req, res) => {
+    data.splice(req.params.updates, 1);
+    res.json(stations);
+})
+
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}.`);
